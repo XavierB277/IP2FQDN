79d140aeca899b17b3f27e8d42df12f791dd4a8e
Xavier Bynum <36524416+XavierB277@users.noreply.github.com>
2019-02-21 15:29:23 -0600

    Add files via upload

---
+++ IP2FQDN.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+from dns import resolver, reversename
+import time
+
+list = []
+for line in open('''your_file_name.txt''', 'r'):
+	list.append(line.strip())
+
+print("\nResolving IP addresses...\n")
+time.sleep(2)
+x = 0
+for ip in list:
+	try:
+		addr = reversename.from_address(list[x])
+		result=str(resolver.query(addr,"PTR")[0])
+		print(list[x] + "\t" + result)
+		x += 1
+	except:
+		print(list[x] + "\t" + "Not Found")
+		x += 1
+
+time.sleep(2)
+print("\nTask Complete!")
